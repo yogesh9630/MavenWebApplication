d05d440a412cca759c1aca184ade8bdb883dec6f
yogesh9630 <66211711+yogesh9630@users.noreply.github.com>
2023-03-21 11:53:02 +0530

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -4,5 +4,6 @@ PR_NUMBER=$(echo "${{ github.event.pull_request.number }}")
 
 echo "The pull request number is: $PR_NUMBER"
 echo "The build number is: $build_number"
+echo "The branch name is: $$BRANCH_NAME"
 
 
